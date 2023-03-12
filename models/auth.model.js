b1c1dde6292d897e5bb6c70e192d23417ccd85d1
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-03-12 14:51:34 +0100

    Add ads and auth mongoose models

---
+++ models/auth.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const authSchema = new mongoose.Schema({
+  login: { type: String, required: true },
+  password: { type: String, required: true },
+  avatar: { type: String, required: true },
+  phone: { type: String, required: true },
+});
+
+module.exports = new mongoose.model('Auth', authSchema);
