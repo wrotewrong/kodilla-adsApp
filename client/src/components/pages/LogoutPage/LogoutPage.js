ece6567e7498c295ce410c2b20085bc5d88c69d9
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-03-22 21:52:59 +0100

    Add logout option

--- client/src/components/pages/LogoutPage/LogoutPage.js
+++ client/src/components/pages/LogoutPage/LogoutPage.js
@@ -1,5 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { API_URL } from '../../../config';
+import { logOut } from '../../../redux/userRedux';
+
 const LogoutPage = () => {
-  return <h1>LogoutPage</h1>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const options = {
+      method: 'DELETE',
+      credentials: 'include',
+    };
+    fetch(`${API_URL}/auth/logout`, options).then(() => {
+      dispatch(logOut());
+    });
+    navigate('/');
+  }, [dispatch]);
+
+  return null;
 };
 
 export default LogoutPage;
--- client/src/redux/userRedux.js
+++ client/src/redux/userRedux.js
@@ -5,11 +5,16 @@ export const getUser = ({ user }) => user.login;
 /* ACTIONS */
 const createActionName = (actionName) => `app/users/${actionName}`;
 const LOG_IN = createActionName('LOG_IN');
+const LOG_OUT = createActionName('LOG_OUT');
 
 /* ACTION CREATORS */
 export const logIn = (payload) => ({
   type: LOG_IN,
   payload,
+});
+
+export const logOut = () => ({
+  type: LOG_OUT,
 });
 
 /* THUNKS */
@@ -26,6 +31,8 @@ export default function reducer(statePart = initialState, action = {}) {
   switch (action.type) {
     case LOG_IN:
       return action.payload;
+    case LOG_OUT:
+      return { user: null };
     default:
       return statePart;
   }
