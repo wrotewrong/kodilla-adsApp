9aeda8d2f77b6f5ed9fb66fdf69d292de71d4b71
Andrzej Popiolek <xandrzejpopiolek@gmail.com>
2023-03-26 16:26:52 +0200

    Add client side AdForm.js validation

--- client/src/components/features/AdForm/AdForm.js
+++ client/src/components/features/AdForm/AdForm.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addAdRequest, editAdRequest } from '../../../redux/adsRedux';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const AddForm = (props) => {
   const { id } = useParams();
@@ -15,8 +16,7 @@ const AddForm = (props) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = () => {
     if (props.action === 'add') {
       dispatch(addAdRequest({ title, text, location, price, img }));
       navigate('/');
@@ -26,21 +26,45 @@ const AddForm = (props) => {
     }
   };
 
+  const {
+    register,
+    handleSubmit: validate,
+    formState: { errors },
+  } = useForm();
+
   return (
-    <Form onSubmit={handleSubmit} className='col-12 col-sm-6  mx-auto'>
+    <Form
+      onSubmit={validate(handleSubmit)}
+      className='col-12 col-sm-6  mx-auto'
+    >
       <Form.Group controlId='formAdd'>
         <Form.Label>Title</Form.Label>
         <Form.Control
+          {...register('title', {
+            required: true,
+            minLength: 10,
+            maxLength: 50,
+          })}
           type='text'
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           placeholder='Enter title'
         ></Form.Control>
+        {errors.title && (
+          <small className='d-block form-text text-danger mt-1'>
+            Title is required and must consist of 10 to 50 characters
+          </small>
+        )}
       </Form.Group>
 
       <Form.Group controlId='formAdd'>
         <Form.Label>Text</Form.Label>
         <Form.Control
+          {...register('text', {
+            required: true,
+            minLength: 20,
+            maxLength: 1000,
+          })}
           as='textarea'
           type='text'
           value={text}
@@ -48,35 +72,66 @@ const AddForm = (props) => {
           placeholder='Enter text'
           style={{ height: '300px' }}
         ></Form.Control>
+        {errors.text && (
+          <small className='d-block form-text text-danger mt-1'>
+            Description is required and must consist of 20 to 1000 characters
+          </small>
+        )}
       </Form.Group>
 
       <Form.Group controlId='formAdd'>
         <Form.Label>Location</Form.Label>
         <Form.Control
+          {...register('location', {
+            required: true,
+          })}
           type='text'
           value={location}
           onChange={(e) => setLocation(e.target.value)}
           placeholder='Enter location'
         ></Form.Control>
+        {errors.location && (
+          <small className='d-block form-text text-danger mt-1'>
+            Location is required
+          </small>
+        )}
       </Form.Group>
 
       <Form.Group controlId='formAdd'>
         <Form.Label>Price</Form.Label>
         <Form.Control
+          {...register('price', {
+            required: true,
+          })}
           type='number'
           value={price}
           onChange={(e) => setPrice(e.target.value)}
           placeholder='Enter price'
         ></Form.Control>
+        {errors.price && (
+          <small className='d-block form-text text-danger mt-1'>
+            Price is required
+          </small>
+        )}
       </Form.Group>
 
       <Form.Group controlId='formAdd'>
         <Form.Label>Picture</Form.Label>
         <Form.Control
+          {...(props.action === 'add' && {
+            ...register('picture', {
+              required: true,
+            }),
+          })}
           type='file'
           onChange={(e) => setImg(e.target.files[0])}
           accept='image/png, image/gif, image/jpeg'
         ></Form.Control>
+        {errors.picture && (
+          <small className='d-block form-text text-danger mt-1'>
+            The image is required
+          </small>
+        )}
       </Form.Group>
 
       <Button type='submit' className='mt-3'>
